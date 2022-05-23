9e50fa6777e89f1621ec231490fc7b6e61cbf7e9
KevinBaoB <kevinbaobui@gmail.com>
2022-05-23 17:01:29 -0500

    first test

--- deaf_grandma.js
+++ deaf_grandma.js
@@ -1,5 +1,24 @@
-function deafGrandma() {
-
+function deafGrandma(input) {
+  console.log(goodbyeCount);
+  if (input.length === 0) {
+    return "WHAT?!";
+  } else if (input === input.toLowerCase()) {
+    return "SPEAK UP, KID!";
+  } else if (input === input.toUpperCase() && input != "GOODBYE!") {
+    return "NO, NOT SINCE 1946!";
+  } else if (input === "GOODBYE!" && goodbyeCount === 0) {
+    goodbyeCount++;
+    return "LEAVING SO SOON?";
+  } else if (input === "GOODBYE!" && goodbyeCount > 0) {
+    return "LATER SKATER!";
+  }
 }
+let goodbyeCount = 0;
 
-deafGrandma();
+console.log("hi, grandma");
+
+console.log("I SAID HI, GRANDMA");
+
+console.log(deafGrandma("GOODBYE!"));
+
+console.log(deafGrandma("GOODBYE!"));
