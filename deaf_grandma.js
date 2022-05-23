643c5bfaf0dd7b308087b1c62fdcd7ca9c026369
KevinBaoB <kevinbaobui@gmail.com>
2022-05-23 18:52:25 -0500

    Figuring Out

--- deaf_grandma.js
+++ deaf_grandma.js
@@ -1,24 +1,19 @@
-function deafGrandma(input) {
-  console.log(goodbyeCount);
-  if (input.length === 0) {
-    return "WHAT?!";
-  } else if (input === input.toLowerCase()) {
-    return "SPEAK UP, KID!";
-  } else if (input === input.toUpperCase() && input != "GOODBYE!") {
-    return "NO, NOT SINCE 1946!";
-  } else if (input === "GOODBYE!" && goodbyeCount === 0) {
-    goodbyeCount++;
-    return "LEAVING SO SOON?";
-  } else if (input === "GOODBYE!" && goodbyeCount > 0) {
-    return "LATER SKATER!";
-  }
-}
+let input = window.prompt("Answer Grandma!");
 let goodbyeCount = 0;
 
-console.log("hi, grandma");
-
-console.log("I SAID HI, GRANDMA");
-
-console.log(deafGrandma("GOODBYE!"));
-
-console.log(deafGrandma("GOODBYE!"));
+// function deafGrandma(input) {
+//   console.log(goodbyeCount);
+if (input.length === 0) {
+  console.log("WHAT?!");
+  input;
+} else if (input === input.toLowerCase()) {
+  console.log("SPEAK UP, KID!");
+} else if (input === input.toUpperCase() && input != "GOODBYE!") {
+  console.log("NO, NOT SINCE 1946!");
+} else if (input === "GOODBYE!" && goodbyeCount === 0) {
+  goodbyeCount++;
+  console.log("LEAVING SO SOON?");
+} else if (input === "GOODBYE!" && goodbyeCount > 0) {
+  console.log("LATER SKATER!");
+}
+// }
